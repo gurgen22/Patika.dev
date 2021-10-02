4e3ccd82d6dd0417fa42469b69a4c2c5837c851d
Kemal Gürgen <77569603+gurgen22@users.noreply.github.com>
2021-10-02 22:10:13 +0300

    Variables added

---
+++ ConsoleProgrammingVariable/Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+
+namespace ConsoleProgrammingVariable
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string schoolName = "";
+            int schoolNumber = 110;
+
+            DateTime dt = DateTime.Now;
+            Console.WriteLine(dt);
+
+            string name = string.Empty;
+            name = "Elif";
+
+            string number1 = "10";
+            int number2 = 20;
+            string newNumber = number1 + number2.ToString();
+            Console.WriteLine(newNumber);
+
+            string dateTime = DateTime.Now.ToString("yy-MMM-dd ddd");
+            string dateTimeHour = DateTime.Now.ToString("hh:mm");
+            Console.WriteLine(dateTime);
+            Console.WriteLine(dateTimeHour);
+        }
+    }
+}
