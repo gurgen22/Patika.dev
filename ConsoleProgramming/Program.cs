cff27dfc1f4d0729f79e8b81e1f1167b832c032d
Kemal Gürgen <77569603+gurgen22@users.noreply.github.com>
2021-10-02 21:10:43 +0300

    Add project files.

---
+++ ConsoleProgramming/Program.cs
@@ -0,0 +1,18 @@
+﻿using System;
+
+namespace ConsoleProgramming
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+            Console.WriteLine("İsim giriniz: ");
+            string name = Console.ReadLine();
+            Console.WriteLine("Soyisim giriniz: ");
+            string surname = Console.ReadLine();
+
+            Console.WriteLine("Merhaba"+" "+name+" "+surname);
+        }           
+    }
+}
