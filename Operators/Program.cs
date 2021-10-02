f7a22bba37465725aa85dd32192779bca7ef8ec8
Kemal Gürgen <77569603+gurgen22@users.noreply.github.com>
2021-10-02 22:33:08 +0300

    Operators

---
+++ Operators/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+
+namespace Operators
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string name = string.Empty;
+           
+            if (name != string.Empty)
+            {
+                Console.WriteLine(name);
+            }
+
+            int number1 = 14;
+            int number2 = 15;
+
+            if (number1 != 0 || number2 != 0)
+            {
+                Console.WriteLine("Değer girilmiş");
+            }
+
+        }
+    }
+}
