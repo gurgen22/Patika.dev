5efd26308539cc8605be2f7ee935cb049ac80447
Kemal Gürgen <77569603+gurgen22@users.noreply.github.com>
2021-10-03 22:03:38 +0300

    Type Conversions

---
+++ TypeConversions/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+
+namespace TypeConversions
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int d = 45;
+            string e = "zikriye";
+            char f = 'k';
+            object g = e + f + d;
+            Console.WriteLine(g);
+
+            int xx = 6;
+            string yy = xx.ToString();
+            Console.WriteLine(yy);
+
+            string sayi1 = "10";
+            string sayi2 = "20";
+            int toplam = Convert.ToInt32(sayi1) + Convert.ToInt32(sayi2);
+            Console.WriteLine(toplam);
+
+        }
+    }
+}
