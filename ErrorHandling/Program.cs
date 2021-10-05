4df09a272cf69e2dd2d815ba56bf371796f46bf2
Kemal Gürgen <77569603+gurgen22@users.noreply.github.com>
2021-10-05 22:28:34 +0300

    ErrorHandling

---
+++ ErrorHandling/Program.cs
@@ -0,0 +1,49 @@
+﻿using System;
+
+namespace ErrorHandling
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            try
+            {
+                Console.WriteLine("Bir sayı giriniz:");
+                int sayi = Convert.ToInt32(Console.ReadLine());
+                Console.WriteLine("Girmiş olduuğunuz sayı: " + " " + sayi);
+            }
+            catch (Exception e)
+            {
+                Console.WriteLine("Hata: " + e.Message.ToString());
+
+            }
+            finally
+            {
+                Console.WriteLine("İşlem tamamlandı.");
+            }
+
+            try
+            {
+                int a = int.Parse(null);
+
+            }
+
+            //catch (Exception e)
+            //{
+            //    Console.WriteLine("Boş değer girdiniz.");
+            //    Console.WriteLine(e);
+
+            //}
+
+            //try
+            //{
+            //    int b = int.Parse("test");
+            //}
+            //catch (ArgumentNullException ex)
+            //{
+            //    Console.WriteLine("Veri tipi uygun değil.");
+            //    Console.WriteLine(ex);
+            //}
+        }
+    }
+}
