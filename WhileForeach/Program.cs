ce6a40ba92fc43e96e702e3b0d42b39a76610a49
Kemal Gürgen <77569603+gurgen22@users.noreply.github.com>
2021-10-10 19:52:28 +0300

    WhileForeach

---
+++ WhileForeach/Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+
+namespace WhileForeach
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+           //1 den başlayarak console dan girilen sayıya kadar (sayı dahil) ortalama hesaplayıp console a yazdıran program.
+           Console.WriteLine("Lütfen bir sayı girin.");
+           int sayi = int.Parse(Console.ReadLine());
+           int sayac = 1;
+           int toplam = 0;
+           while (sayac<=sayi)
+           {
+               toplam += sayac;
+               sayac++;
+           }
+
+           Console.WriteLine(toplam/sayi);
+
+           string[] cars = {"Ford", "Renault", "Toyota"};
+           foreach (var car in cars)
+           {
+               Console.WriteLine(car);
+           }
+        }
+    }
+}
