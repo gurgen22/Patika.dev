c628b18ccf21f6ae484fbe707b56a6c46ead737b
Kemal Gürgen <77569603+gurgen22@users.noreply.github.com>
2021-10-10 22:41:39 +0300

    ArrayMethods

---
+++ ArrayMethods/Program.cs
@@ -0,0 +1,32 @@
+﻿using System;
+
+namespace ArrayMethods
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int[] sayiDizisi = {1, 55, 4, 33, 62, 78};
+            foreach (var sayi     in sayiDizisi)
+                Console.WriteLine(sayi);
+            
+
+            Array.Sort(sayiDizisi);
+            foreach (var sayi in sayiDizisi)
+                Console.WriteLine(sayi);
+            
+
+            Array.Clear(sayiDizisi, 2, 1);
+            foreach (var sayi in sayiDizisi)
+                Console.WriteLine(sayi);
+
+            Array.Reverse(sayiDizisi);
+            foreach (var sayi in sayiDizisi)
+                Console.WriteLine(sayi);
+
+            Console.WriteLine(Array.IndexOf(sayiDizisi, 78));
+
+
+        }
+    }
+}
