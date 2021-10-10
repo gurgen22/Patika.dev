d1f5e59b07795ef519926ac940aeb154c6cd2de9
Kemal Gürgen <77569603+gurgen22@users.noreply.github.com>
2021-10-10 17:15:52 +0300

    SwitchCase

---
+++ Enum/Program.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Linq;
+
+public class Example
+{
+    public class Modules
+    {
+        public Guid ID { get; set; }
+        public string name { get; set; }
+        public int tableNo { get; set; }
+    }
+    public enum Translation
+    {
+        English = 1,
+        Russian = 2,
+        French = 4,
+        German = 5
+    }
+
+    public static void Main()
+    {
+        //Modules modules = new();
+        //string[] trans = Enum.GetNames(typeof(Translation));
+
+        //var list = new List<KeyValuePair<string, int>>();
+    }
+    public List<KeyValuePair<string, int>> GetEnumList<Translation>()
+    {
+        Modules modules = new();
+        var list = new List<KeyValuePair<string, int>>();
+        foreach (var e in Enum.GetValues(typeof(Translation)))
+        {
+            list.Add(new KeyValuePair<string, int>(e.ToString(), (int)e));
+        }
+        return list;
+
+
+      
+    }
+}
---
+++ SwitchCase/Program.cs
@@ -0,0 +1,39 @@
+﻿using System;
+
+namespace SwitchCase
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int month = DateTime.Now.Month;
+            switch (month)
+            {
+                case 1:
+                    Console.WriteLine("Ocak ayındasınız.");
+                    break;
+                case 2:
+                    Console.WriteLine("Şubat ayındasınız.");
+                    break;
+                case 10:
+                    Console.WriteLine("Ekim ayındasınız.");
+                    break;
+                default:
+                    Console.WriteLine("Yanlış veri girişi");
+                    break;
+            }
+
+            switch (month)
+            {
+                case 9:
+                case 10:
+                case 11:
+                    Console.WriteLine("Sonbahar mevsimindesiniz.");
+                    break;
+                default:
+                    break;
+
+            }
+        }
+    }
+}
