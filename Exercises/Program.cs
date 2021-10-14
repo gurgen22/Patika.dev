2c280a926bcb1cdb79d00ed3a721a07bb449b4bd
Kemal Gürgen <77569603+gurgen22@users.noreply.github.com>
2021-10-14 09:08:06 +0300

    Exercises1

---
+++ Exercises/Program.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace Exercises
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            /*-------------------------------------------------------------- 1 ------------------------------------------------------*/
+            Console.WriteLine("Pozitif bir tam sayı giriniz.");
+            int sayi = Convert.ToInt32(Console.ReadLine());
+
+            List<int> sayilar = new List<int>();
+            for (int i = 0; i < sayi; i++)
+            {
+                Console.WriteLine("Pozitif tam sayı giriniz.");
+                int gelenSayi = Convert.ToInt32(Console.ReadLine());
+                sayilar.Add(gelenSayi);
+            }
+
+            foreach (var item in sayilar)
+            {
+                if (item % 2 == 0)
+                {
+                    Console.WriteLine(item);
+                }
+            }
+            /*-------------------------------------------------------------- 2 ------------------------------------------------------*/
+            /*-------------------------------------------------------------- 3 ------------------------------------------------------*/
+            /*-------------------------------------------------------------- 4 ------------------------------------------------------*/
+        }
+    }
+}
