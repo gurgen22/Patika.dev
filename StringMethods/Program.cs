f674a684594af9643534a1e025dc58a81a5e44da
Kemal Gürgen <77569603+gurgen22@users.noreply.github.com>
2021-10-18 17:09:37 +0300

    StringMethods

---
+++ StringMethods/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+
+namespace StringMethods
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string degisken = "Merhaba";
+            string degisken2 = "Dersimiz C#";
+            //Padleft, PadRight;
+            Console.WriteLine(degisken.PadRight(15,'*')+ degisken2);
+
+            // Split
+            Console.WriteLine(degisken2.Split(' ')[0]);
+
+            //Substring
+            Console.WriteLine(degisken.Substring(4));
+            Console.WriteLine(degisken2.Substring(1,6));
+        }
+    }
+}
