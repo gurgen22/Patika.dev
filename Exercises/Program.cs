6b07fbb84c4e87c2c30ca0d3cd10218ba2d7bdbc
Kemal Gürgen <77569603+gurgen22@users.noreply.github.com>
2021-10-19 10:53:26 +0300

    Exercises

--- Exercises/Program.cs
+++ Exercises/Program.cs
@@ -1,5 +1,7 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.IO;
+using System.Net.NetworkInformation;
 
 namespace Exercises
 {
@@ -7,28 +9,88 @@ namespace Exercises
     {
         static void Main(string[] args)
         {
-            /*-------------------------------------------------------------- 1 ------------------------------------------------------*/
-            Console.WriteLine("Pozitif bir tam sayı giriniz.");
-            int sayi = Convert.ToInt32(Console.ReadLine());
-
-            List<int> sayilar = new List<int>();
-            for (int i = 0; i < sayi; i++)
+            
+            Console.WriteLine("Ödev numarasını giriniz.");
+            int odev = Convert.ToInt32(Console.ReadLine());
+            if (odev==1)
             {
-                Console.WriteLine("Pozitif tam sayı giriniz.");
-                int gelenSayi = Convert.ToInt32(Console.ReadLine());
-                sayilar.Add(gelenSayi);
+                Console.WriteLine("Pozitif bir tam sayı giriniz.");
+                int sayi = Convert.ToInt32(Console.ReadLine());
+
+                List<int> sayilar = new List<int>();
+                for (int i = 0; i < sayi; i++)
+                {
+                    Console.WriteLine("Pozitif tam sayı giriniz.");
+                    int gelenSayi = Convert.ToInt32(Console.ReadLine());
+                    sayilar.Add(gelenSayi);
+                }
+
+                foreach (var item in sayilar)
+                {
+                    if (item % 2 == 0)
+                    {
+                        Console.WriteLine(item);
+                    }
+                }
             }
 
-            foreach (var item in sayilar)
+           
+           
+            else if(odev==2)
             {
-                if (item % 2 == 0)
+                Console.WriteLine("Pozitif tam sayı giriniz.");
+                int n = Convert.ToInt32(Console.ReadLine());
+
+                Console.WriteLine("Pozitif tam sayı giriniz.");
+                int m = Convert.ToInt32(Console.ReadLine());
+
+                List<int> DegerListesi = new List<int>();
+                for (int i = 0; i < n; i++)
+                {
+                    Console.WriteLine("Sayı giriniz.");
+                    int gelenDeger = Convert.ToInt32(Console.ReadLine());
+                    DegerListesi.Add(gelenDeger);
+                }
+
+                for (int j = 0; j < DegerListesi.Count; j++)
+                {
+                    if (j == m || j % m == 0)
+                    {
+                        Console.WriteLine(j);
+                    }
+                }
+            }
+           else if (odev == 3)
+            {
+                Console.WriteLine("Pozitif bir sayı giriniz.");
+                int n = Convert.ToInt32(Console.ReadLine());
+
+                List<string> deger = new List<string>();
+                for (int i = 0; i < n; i++)
+                {
+                    Console.WriteLine("Kelime giriniz.");
+                    string gelenDeger = Console.ReadLine();
+                    deger.Add(gelenDeger);
+                }
+                deger.Reverse();
+                foreach (var item in deger)
                 {
                     Console.WriteLine(item);
                 }
             }
-            /*-------------------------------------------------------------- 2 ------------------------------------------------------*/
-            /*-------------------------------------------------------------- 3 ------------------------------------------------------*/
-            /*-------------------------------------------------------------- 4 ------------------------------------------------------*/
-        }
+            else if (odev == 4)
+            {
+                
+                Console.WriteLine("1 cümle giriniz.");
+                string gelenDeger = Console.ReadLine();
+               string[] liste = gelenDeger.Split(" ");
+               Console.WriteLine("Kelime sayısı: "+liste.Length);
+               Console.WriteLine("Karakter sayısı: "+gelenDeger.Length);
+            }
+            else
+            {
+                Console.WriteLine("Hatalı işlem yapıldı.");
+            }
+            
     }
 }
---
+++ dictionary/Program.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace dictionary
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Dictionary<int, string> kullanıcılar = new Dictionary<int, string>();
+            kullanıcılar.Add(1,"Ayşe Yılmaz");
+            kullanıcılar.Add(11,"Ahmet Yılmaz");
+            kullanıcılar.Add(18,"Ada Coşar");
+            kullanıcılar.Add(21,"Özcan Coşar");
+
+            //Erişim
+
+            Console.WriteLine(kullanıcılar[11]);
+            foreach (var item in kullanıcılar)
+            {
+                Console.WriteLine(item);
+            }
+
+            //Count
+            Console.WriteLine(kullanıcılar.Count);
+
+            //contains
+            Console.WriteLine(kullanıcılar.ContainsKey(11));
+            Console.WriteLine(kullanıcılar.ContainsValue("Zikriye Ürkmez"));
+
+            //Remove
+            Console.WriteLine(kullanıcılar.Remove(11));
+            foreach (var item in kullanıcılar)
+            {
+                Console.WriteLine(item);
+            }
+        }
+    }
+}
